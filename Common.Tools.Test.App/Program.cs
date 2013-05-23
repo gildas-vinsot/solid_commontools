863af3dbe2d2633e2eecd5dcb05139bf472a20c8
gildas portable <g.vinsot@moodmedia.com>
2013-05-23 15:59:31 +0200

    Added new example for SOLID principles

---
+++ Common.Tools.Test.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools.Test.Hash;
+
+namespace Common.Tools.Test.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTest test = new HashTest();
+            test.TestMethodGetHashCode();
+        }
+    }
+}
